db2478766658ce6eec5e66ddab8f754602d6e641
Knight A <126755960+aegisu@users.noreply.github.com>
2023-03-13 11:07:01 +0800

    Add files via upload

---
+++ AA2script.js
@@ -0,0 +1,8 @@
+//loops
+
+//while
+//do while
+//for loop
+
+//if + is used to bind strings together it is a concatenation operator
+//if + is used for a mathematical operation it is an addition operator
